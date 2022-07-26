eff46d7fef71da80afe01a525f53de434affbca5
Elad Kishon <99413726+eladkishon@users.noreply.github.com>
2022-07-26 23:50:59 +0300

    Update developer-utilities.sh

--- scripts/common/developer-utilities.sh
+++ scripts/common/developer-utilities.sh
@@ -2,5 +2,10 @@ echo
 
 # Don't exit if any of these fail
 set +e
-  brew install --cask insomnia # api interaction tool
+  brew install --cask insomnia # api
+  brew install --cask docker
+  brew install dive
+  
+  brew install --cask figma
+
 set -e
