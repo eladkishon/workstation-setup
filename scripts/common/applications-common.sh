c9cb3f934bb252da82a2c1867a6c8f3fca62dc36
Elad Kishon <99413726+eladkishon@users.noreply.github.com>
2022-07-26 23:27:26 +0300

    Update applications-common.sh

--- scripts/common/applications-common.sh
+++ scripts/common/applications-common.sh
@@ -7,16 +7,13 @@ echo "Installing applications"
 
 # Utilities
 
-brew install --cask flycut
 brew install --cask rectangle
 
 # Terminals
 brew install --cask iterm2
 
 # Browsers
-
 brew install --cask google-chrome
-brew install --cask firefox
 
 # Communication
 brew install --cask slack
