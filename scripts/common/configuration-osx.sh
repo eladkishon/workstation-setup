16525ecb5e94a33fa7ac5cc3ca443b32699d9360
Elad Kishon <99413726+eladkishon@users.noreply.github.com>
2022-07-26 23:29:33 +0300

    Update configuration-osx.sh

--- scripts/common/configuration-osx.sh
+++ scripts/common/configuration-osx.sh
@@ -3,12 +3,12 @@ echo 'Customizing OS X configuration'
 
 # set menu clock
 # see http://www.unicode.org/reports/tr35/tr35-31/tr35-dates.html#Date_Format_Patterns
-defaults write com.apple.menuextra.clock "DateFormat" 'EEE MMM d  h:mm:ss a'
+defaults write com.apple.menuextra.clock "DateFormat" 'EEE MMM d  h:mm a'
 killall SystemUIServer
 
 # hide the dock
-defaults write com.apple.dock autohide -bool true
-killall Dock
+# defaults write com.apple.dock autohide -bool true
+# killall Dock
 
 # fast key repeat rate, requires reboot to take effect
 defaults write ~/Library/Preferences/.GlobalPreferences KeyRepeat -int 1
@@ -41,5 +41,3 @@ echo "Configuring Rectangle"
 cp files/com.knollsoft.Rectangle.plist ~/Library/Preferences/com.knollsoft.Rectangle.plist
 open /Applications/Rectangle.app
 
-echo "Configuring FlyCut"
-open /Applications/Flycut.app
